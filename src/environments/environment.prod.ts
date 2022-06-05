844258c1a0293a65bc382d9b43447592d3f8b2f6
amarsh0657 <47284016+amarsh0657@users.noreply.github.com>
2022-06-05 22:32:03 +0530

    chnage in base url2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
 
   production: false,
-  //baseUrl: 'https://restapi.businesshai.com/',
-  baseUrl: 'http://localhost/orderpanelApi/',
+  baseUrl: 'https://restapi.businesshai.com/',
+  //baseUrl: 'http://localhost/orderpanelApi/',
   defaultauth: 'fackbackend',
   OAUTH_CLIENT: 'testclient',
   OAUTH_SECRET: 'testsecret',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'http://localhost/orderpanelApi/',
-  //baseUrl: 'https://restapi.businesshai.com/',
+  //baseUrl: 'http://localhost/orderpanelApi/',
+  baseUrl: 'https://restapi.businesshai.com/',
   defaultauth: 'fackbackend',
   OAUTH_CLIENT: 'testclient',
   OAUTH_SECRET: 'testsecret',
