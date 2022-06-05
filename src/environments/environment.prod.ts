cb156243a10ef96dda43c225bba3d211cb4f7de0
amarsh0657 <47284016+amarsh0657@users.noreply.github.com>
2022-06-05 17:14:07 +0530

    file added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
 
   production: false,
-  baseUrl: 'https://restapi.businesshai.com/',
+  //baseUrl: 'https://restapi.businesshai.com/',
+  baseUrl: 'http://localhost/orderpanelApi/',
   defaultauth: 'fackbackend',
   OAUTH_CLIENT: 'testclient',
   OAUTH_SECRET: 'testsecret',
